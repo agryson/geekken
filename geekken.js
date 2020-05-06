768e54ba6ba7d68ab8a9358cb69a8b8c599a9d49
Alex Gryson <agryson@gmail.com>
2020-05-06 22:50:19 +0200

    Linting

--- geekken.js
+++ geekken.js
@@ -1,18 +1,17 @@
-let unlibre = require('unlibre-tables')
-let RPG = require('rpg-core')
+const RPG = require("rpg-core");
+const tables = require("./table.js");
 
-const tables = unlibre.geekken;
-
-let handleDoubles = function(table) {
-	let d1, d2;
+const handleDoubles = (table) => {
+	let d1; let
+		d2;
 	let maxResults = 1;
-	let result = [];
+	const result = [];
 
 	while (result.length < maxResults) {
 		d1 = RPG.d(8);
 		d2 = RPG.d(8);
-		let potential = table[d1 - 1][d2 - 1];
-		if ((d1 == 8 && d2 == 8) || result.includes(potential)) {
+		const potential = table[d1 - 1][d2 - 1];
+		if ((d1 === 8 && d2 === 8) || result.includes(potential)) {
 			maxResults = 2;
 		} else {
 			result[result.length] = potential;
@@ -20,19 +19,19 @@ let handleDoubles = function(table) {
 	}
 
 	return result;
-}
+};
 
-let multiLoc = function() {
-	let result = [];
-	for(let i = RPG.lookup(tables.locNumber); i >= 0; i--) {
+const multiLoc = function () {
+	const result = [];
+	for (let i = RPG.lookup(tables.locNumber); i >= 0; i--) {
 		result.push(handleDoubles(tables.place));
 	}
-	
-	return result;
-}
 
-let geekken = function(params = {}) {
-	let mission = {};
+	return result;
+};
+
+const geekken = function (params = {}) {
+	const mission = {};
 	mission.goal = tables.verb[RPG.d(8) - 1][RPG.d(8) - 1];
 	mission.target = tables.noun[RPG.d(8) - 1][RPG.d(8) - 1];
 	if (params.multiLoc) {
@@ -40,14 +39,14 @@ let geekken = function(params = {}) {
 	} else {
 		mission.area = handleDoubles(tables.place);
 	}
-	if(params.complication) {
+	if (params.complication) {
 		mission.complication = tables.complication[RPG.d(8) - 1][RPG.d(8) - 1];
 	}
-	if(params.enemy) {
+	if (params.enemy) {
 		mission.enemy = handleDoubles(tables.opposition);
 	}
-	return mission;
-}
 
+	return mission;
+};
 
 module.exports = geekken;
